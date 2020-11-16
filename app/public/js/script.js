a505a2bec8238275fbf01af7d043696e1d64c9f7
Michael Carter <mjc3bb@gmail.com>
2020-11-15 22:47:42 -0500

    Add saving and new routes

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,4 +1,28 @@
+let quill;
+
 let renderQuill = () => {
-    var quill = new Quill('#editor', { theme: 'snow' });
+    quill = new Quill('#editor', {theme: 'snow'});
+    if (!!delta) {
+        quill.setContents(delta)
+    }
 }
 
+const saveFile = async () => {
+    const delta = quill.getContents()
+    try {
+        const response = await fetch("/save", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(delta)
+        })
+        const data = await response.json()
+        window.location.href = `/pasta/${data.id}`
+    }catch (e){
+        console.error(e)
+        throw e
+    }
+}
+
+document.getElementById("b_save").addEventListener("click", saveFile)
--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,49 @@
-const express = require('express');
-const pug = require('pug');
-const app = express();
-const port = 3000;
+const express = require('express')
+var bodyParser = require('body-parser')
+const pug = require('pug')
+const app = express()
+const mongoose = require('mongoose')
+const port = 3000
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+let connection = mongoose.connect("mongodb://root:example@mongo:27017/copypasta?authSource=admin", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+mongoose.set('useCreateIndex', true)
 
-app.use(express.static('public'));
+const Pasta = mongoose.model("Pasta", {
+    name: {type: String, default: ""},
+    language: {type: String, default: "Plain Text"},
+    delta: Object,
+})
 
-app.get('/', (req, res) => {
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.json())
+
+app.get('/', async (req, res) => {
     res.render('pasta', { message: 'Hello there!'})
 })
 
+app.get('/pasta/:pastaID', async (req, res) => {
+    const p = await Pasta.findById(req.params.pastaID).lean()
+    res.render('pasta', { message: 'Hello there!', delta: JSON.stringify(p.delta)})
+})
+
+app.post('/save', async (req, res) => {
+    const delta = req.body
+    const _id = mongoose.Types.ObjectId();
+    const p = new Pasta({
+        _id,
+        delta,
+    })
+    await p.save()
+    res.send( {id: _id})
+})
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`)
 })
